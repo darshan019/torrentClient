b7e5b3f71c3d40af916acb8d980251e21545d183
darshan019 <darsha019@gmail.com>
2025-04-14 07:16:02 +0000

    starting template

---
+++ file/torrentfile.go
@@ -0,0 +1,38 @@
+package file
+
+import (
+	"io"
+
+	"github.com/jackpal/bencode-go"
+)
+
+type TorrentFile struct {
+	Announce    string
+	InfoHash    [20]byte
+	PieceHashes [][20]byte
+	PieceLength int
+	Length      int
+	Name        string
+}
+
+type bencodeInfo struct {
+	Pieces       string `bencode:"pieces"`
+	PiecesLength string `bencode:"piece length"`
+	Length       int    `bencode:"length"`
+	Name         string `bencode:"name"`
+}
+
+type bencodeTorrent struct {
+	Announce string      `bencode:"announce"`
+	Info     bencodeInfo `bencode:"info"`
+}
+
+func Open(r io.Reader) (*bencodeTorrent, error) {
+	bto := bencodeTorrent{}
+	err := bencode.Unmarshal(r, &bto)
+	if err != nil {
+		return nil, err
+	}
+
+	return &bto, nil
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module torrentClient
+
+go 1.22.4
+
+require github.com/jackpal/bencode-go v1.0.2
