0ff2f8d0031a1e663ec0c4acd6d456cbdf883908
darshan019 <darsha019@gmail.com>
2025-05-29 16:41:42 +0530

    parsing of torrent file finished

--- file/torrentfile.go
+++ file/torrentfile.go
@@ -1,7 +1,12 @@
 package file
 
 import (
+	"bytes"
+	"crypto/sha1"
+	"fmt"
 	"io"
+
+	// "file/test"
 
 	"github.com/jackpal/bencode-go"
 )
@@ -16,10 +21,10 @@ type TorrentFile struct {
 }
 
 type bencodeInfo struct {
-	Pieces       string `bencode:"pieces"`
-	PiecesLength string `bencode:"piece length"`
-	Length       int    `bencode:"length"`
-	Name         string `bencode:"name"`
+	Pieces      string `bencode:"pieces"`
+	PieceLength int    `bencode:"piece length"`
+	Length      int    `bencode:"length"`
+	Name        string `bencode:"name"`
 }
 
 type bencodeTorrent struct {
@@ -35,4 +40,53 @@ func Open(r io.Reader) (*bencodeTorrent, error) {
 	}
 
 	return &bto, nil
+}
+
+func (i *bencodeInfo) hash() ([20]byte, error) {
+	var buf bytes.Buffer
+	err := bencode.Marshal(&buf, *i)
+	if err != nil {
+		return [20]byte{}, err
+	}
+
+	h := sha1.Sum(buf.Bytes())
+	return h, nil
+}
+
+func (i *bencodeInfo) splitPieceHashes() ([][20]byte, error) {
+	hashLen := 20 // Length of SHA-1 hash
+	buf := []byte(i.Pieces)
+	if len(buf)%hashLen != 0 {
+		err := fmt.Errorf("Received malformed pieces of length %d", len(buf))
+		return nil, err
+	}
+	numHashes := len(buf) / hashLen
+	hashes := make([][20]byte, numHashes)
+
+	for j := 0; j < numHashes; j++ {
+		copy(hashes[j][:], buf[j*hashLen:(j+1)*hashLen])
+	}
+
+	return hashes, nil
+}
+
+func (bto *bencodeTorrent) ToTorrentFile() (TorrentFile, error) {
+	infoHash, err := bto.Info.hash()
+	if err != nil {
+		return TorrentFile{}, err
+	}
+
+	pieceHashes, err := bto.Info.splitPieceHashes()
+	if err != nil {
+		return TorrentFile{}, err
+	}
+	t := TorrentFile{
+		Announce:    bto.Announce,
+		InfoHash:    infoHash,
+		PieceHashes: pieceHashes,
+		PieceLength: bto.Info.PieceLength,
+		Length:      bto.Info.Length,
+		Name:        bto.Info.Name,
+	}
+	return t, nil
 }
--- main.go
+++ main.go
@@ -2,8 +2,33 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"torrentClient/file"
 )
 
 func main() {
 	fmt.Println("hello")
+
+	f, err := os.Open("test.torrent")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	bto, err := file.Open(f)
+	if err != nil {
+		panic(err)
+	}
+
+	torrent, err := bto.ToTorrentFile()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Announce URL:", torrent.Announce)
+	fmt.Println("File Name:", torrent.Name)
+	fmt.Println("File Length:", torrent.Length)
+	fmt.Println("Piece Length:", torrent.PieceLength)
+	fmt.Println("Info Hash:", torrent.InfoHash)
+	fmt.Println("Number of Pieces:", len(torrent.PieceHashes))
 }
